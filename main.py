afcc61ffefe0b89e9c8504d3098d0d8a9151b292
Kamran433 <102954239+Kamran433@users.noreply.github.com>
2022-11-02 01:26:27 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+# import treasure_logo
+# print(treasure_logo.logo)
+# print("Welcome to Treasure Island.")
+# print("Your mission is to find the treasure.")
+# print("You're at a crossroad. Where do you want to go?")
+# direct=input("Type 'left' or 'right'\n")
+# if direct=="right":
+#   print("GAME OVER!")
+# elif direct=="left":
+#   print("You've come to a lake.There is an island in the middle of the lake.")
+#   direct2=input('Type "wait" to wait for a boat. Type "swim" to swim across.\n')
+#   if direct2=="swim":
+#     print("GAME OVER")
+#   elif direct2=="wait":
+#     print("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue.")
+#     direct3=input("Which colour do you choose?\n")
+#     if direct3=="red" :
+#       print("It's a room full of fire. Game Over.")
+#     elif direct3=="blue":
+#       print("You enter a room of beasts. Game Over.")
+#     elif direct3=="yellow":
+#       print("You found the treasure! You Win!")
+#     else:
+#       print("You chose a door that doesn't exist. Game Over.")
